a2064675cdff5cc904ce13263091ad3bfa04134c
holy <holy1017@naver.com>
2020-08-29 16:53:53 +0900

    ++

--- cpp-holy/cpp-holy.cpp
+++ cpp-holy/cpp-holy.cpp
@@ -7,38 +7,86 @@ using namespace std;
 int cinInt();
 char* cinChar();
 
+namespace my {
+    int c = 4;
+}
+
 int main()
 {
+    int 한글 = 0; // C++11
+    {
+        int v;
+        v = 15;//
+        std::cout << v << "\n";
+        v = 0b1010;//2진
+        std::cout << v << "\n";
+        v = 023;//8진
+        std::cout << v << "\n";
+        v = 0x2f;//16진
+        std::cout << v << "\n";
+    }
+    {
+        long long l;
+        l = 321;
+        std::cout << l << "\n";
+        l = 321u;//unsigned
+        std::cout << l << "\n";
+        l = 321L;//long
+        std::cout << l << "\n";
+        l = 321uL;//unsigned long
+        std::cout << l << "\n";
+        l = 321ll;//long long
+        std::cout << l << "\n";
+    }
+    {
+        std::cout << 'A' << "\n";//ASC2
+        std::cout << '\101' << "\n";//8진
+        std::cout << '\x41' << "\n";//16진
+    }
+    {
+        std::cout << 12. << "\n";// 더블
+        std::cout << 12.0 << "\n";// 더블
+        std::cout << 12e0 << "\n";// 더블
+        std::cout << 1.2e+3 << "\n";// 더블
+
+        std::cout << 12.0f << "\n";//
+        std::cout << 12e2f << "\n";//
+    }
+
     std::cout << "Hello World!\n";
     std::cout << "Hello World!" << std::endl;
-    int a = 1, b = 0;
-    if (a>b)
     {
-        cout<< a<<endl;
-    }
-    else
-    {
-        cout<< b<<endl;
+        int b = 0;
+        if (my::c > b)
+        {
+            cout << my::c << endl;
+        }
+        else
+        {
+            cout << b << endl;
+        }
     }
     //------------------------------
-    int i;
-    char s[1024];
-    cout << "Enter : ";
-    cin >> i >> s;
-    if (cin.fail())
-        cout << "Wrong input." << endl; // 에러 메시지 출력
-    else
     {
-    cout << "i:" << i << endl;
-    cout << "s:" << s << endl;
+        int i;
+        char s[1024];
+        cout << "Enter : ";
+        cin >> i >> s;
+        if (cin.fail())
+            cout << "Wrong input." << endl; // 에러 메시지 출력
+        else
+        {
+            cout << "i:" << i << endl;
+            cout << "s:" << s << endl;
+        }
+        cin.clear(); // 오류스트림을 초기화
+        cin.ignore(256, '\n'); // 입력버퍼를 비움
     }
-    cin.clear(); // 오류스트림을 초기화
-    cin.ignore(256, '\n'); // 입력버퍼를 비움
-
     //------------------------------
-    cout << "cinInt:" << cinInt() << endl;
-    cout << "cinChar:" << cinChar() << endl;
-
+    {
+        cout << "cinInt:" << cinInt() << endl;
+        cout << "cinChar:" << cinChar() << endl;
+    }
     return 0;
 }
 
